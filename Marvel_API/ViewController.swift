a351fcd8679416e7b9c2af85f488f2a6d0e76d0a
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2022-08-01 15:24:35 +0400

    Initial Commit

---
+++ Marvel_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel_API
+//
+//  Created by Евангелина Клюкай on 01.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
